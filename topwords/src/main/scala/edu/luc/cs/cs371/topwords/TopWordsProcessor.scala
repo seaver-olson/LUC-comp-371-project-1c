9d91e480fc6ffe61be095b9cf32bc4ba358a55f1
Seaver Olson <sjolson007@gmail.com>
2026-02-20 11:50:00 -0600

    finished main algorithm

--- topwords/src/main/scala/edu/luc/cs/cs371/topwords/TopWordsProcessor.scala
+++ topwords/src/main/scala/edu/luc/cs/cs371/topwords/TopWordsProcessor.scala
@@ -1,11 +1,9 @@
 package edu.luc.cs.cs371.topwords
 
-import scala.collection.mutable
-
 //i found that case classes have a lot of useful builtin methods as well as being immutable by default
 //https://stackoverflow.com/questions/44607744/scala-case-classes-are-they-just-structs#:~:text=albeit%20a%20case%20class%20is%20immutable%20by%20default%2C
 case class WindowState(
-  window: List[String],//replacement for mutable Queue
+  window: Vector[String],//replacement for mutable Queue, Vector is faster than List : https://www.scala-lang.org/docu/files/collections-api/collections_15.html
   freqs: Map[String, Int],//replacement for mutable Map
   wordCount: Int
 )
@@ -15,9 +13,6 @@ object WindowState:
     WindowState = WindowState(List.empty, Map.empty, 0)
 //stdin args + Observer Object 
 object TopWordsProcessor:
-  private val window = mutable.Queue[String]()//FIFO
-  private val freqs = mutable.Map[String, Int]().withDefaultValue(0)
-  private var stepCounter = 0;
   /**
    * Filter words shorter than minLength
    * Add word to window and update freqs
@@ -37,7 +32,7 @@ object TopWordsProcessor:
   //since windowState is immutable to updateState I will create a new instance of the WindowState with the new word
   def updateState(state: WindowState, newWord: String, windowSize: Int): WindowState = 
     val updatedWindow = state.window :+ newWord//:+ == append to end of list
-    val updatedFreqs = state.freqs.updatedWith(word):
+    val updatedFreqs = state.freqs.updatedWith(newWord):
       case Some(count) => Some(count + 1)//if something is found in map then update frequency
       case None => Some(1)//else start the count
     if updatedWindow.size > windowSize then
@@ -45,15 +40,15 @@ object TopWordsProcessor:
       val evictedWord = updatedWindow.head
       val afterEvict = updatedWindow.tail//strange method but okay https://www.geeksforgeeks.org/scala/scala-stack-tail-method-with-example/#:~:text=It%20returns%20a%20new%20stack%20that%20consists%20of%20all%20the%20elements%20except%20the%20first%20one.
       //re-eval freqs
-      val trimmedFreqs = updatedFreqs.updatedWith(oldestWord): //either subtract one or remove freq count
+      val trimmedFreqs = updatedFreqs.updatedWith(evictedWord): //either subtract one or remove freq count
         case Some(count) if count > 1 => Some(count -1)
         case _ => None
       
       WindowState(afterEvict, trimmedFreqs, state.wordCount + 1)
     else 
       WindowState(updatedWindow, updatedFreqs, state.wordCount + 1)
-  private def generateWordCloud(): List[(String, Int)] =
-    freqs
+  private def generateWordCloud(state: WindowState, cloudSize: Int): List[(String, Int)] =
+    state.freqs
       .toList
       .sortBy {case (word, freq) => (-freq, word)}//sort by freq desc 
       .take(cloudSize)
--- topwords/src/main/scala/edu/luc/cs/cs371/topwords/WordCloudObserver.scala
+++
@@ -1,7 +0,0 @@
-package edu.luc.cs.cs371.topwords
-
-//Observer to receive word cloud updates
-trait WordCloudObserver:
-  //WordCloud should be a list of (word, freq)
-  def onUpdate(wordCloud: List[(String, Int)]): Unit 
-
